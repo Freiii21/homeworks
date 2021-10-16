d11924c1d9fb7dcfef477390b7e3d02dedabba75
Freiii21 <mrfreiii@gmail.com>
2021-10-16 15:01:01 +0300

    #42 Ignat homework#1_1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Message from './Message';
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGC7fyAbbc8TCLxm3SvPc2IOxlonq80CaP5A&usqp=CAU',
+    name: 'Hans Peter Geerdes',
+    message: 'How much is the Fish? How much is the Fish?How much is the Fish',
     time: '22:00',
 }
 
@@ -11,16 +11,12 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.avatar}><img src={props.avatar}/></div>
+            <div className={s.name}>{props.name}</div>
+            <div className={s.text}>{props.message}</div>
+            <div className={s.time}>{props.time}</div>
         </div>
     )
 }
