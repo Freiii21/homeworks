77600d810452f7a45015c6e71662554854b3aef9
Freiii21 <mrfreiii@gmail.com>
2021-10-21 21:31:05 +0300

    #42 Ignat homework#2_2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 import Affair from './Affair'
 import {AffairType, FilterType} from './HW2'
 
-type AffairsPropsType = { // need to fix any
-    data: (affairs:Array<AffairType>, filter:FilterType) => void
-    setFilter: any
-    deleteAffairCallback: any
+type AffairsPropsType = {
+    data: Array<AffairType>
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+        <Affair
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {props.setFilter("all")}
+    const setHigh = () => {props.setFilter("high")}
+    const setMiddle = () => {props.setFilter("middle")}
+    const setLow = () => {props.setFilter("low")}
 
     return (
         <div>
