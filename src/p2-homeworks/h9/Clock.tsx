d80672d1b0f1c3c368a35461889f5e7f2f521210
Freiii21 <mrfreiii@gmail.com>
2021-12-14 21:46:15 +0300

    #42 Ignat homework_9_2

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {brotliCompress} from 'zlib';
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,40 +8,45 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId);
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true);
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false);
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/>
+    const stringDate = date?.toLocaleDateString() || <br/>
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+            <div style={{marginLeft: '10px',marginBottom:"10px",fontWeight:"bold", color:"green",width:"85px"}}>
+                <div
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
 
-            {show && (
-                <div>
-                    {stringDate}
+                >
+                    {stringTime}
                 </div>
-            )}
+
+                {show ? (
+                    <div>
+                        {stringDate}
+                    </div>
+                ) : (
+                    <br/>
+                )}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
